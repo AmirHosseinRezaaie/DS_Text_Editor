b97b785f88ce6141120624194432ced3e6d3c812
ah.rezaaie <ah.rezaaie@gmail.com>
2026-02-03 17:30:08 +0330

    Compelete phase3 Add copy-paste opration

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,17 @@
 
 using namespace std;
 
-string text;    
+struct Action {
+    string type; 
+    size_t position;
+    string content;
+    size_t length;      
+};
+
+string text;  
+stack<Action> undoStack;
+stack<Action> redoStack;
+string clipboard;       
 
 void print() {
     cout << text << endl;
@@ -39,8 +49,15 @@ void doInsert(size_t pos, const string& content) {
         cout << "Invalid position!" << endl;
         return;
     }
-    // text.insert(pos, content);
-    manual_insert(pos, content);
+
+    Action act;
+    act.type = "insert";
+    act.position = pos;
+    act.content = content;
+    undoStack.push(act);
+    redoStack = stack<Action>();
+
+    text.insert(pos, content);
     print();
 }
 
@@ -49,13 +66,90 @@ void doDelete(size_t pos, size_t len) {
         cout << "Invalid range!" << endl;
         return;
     }
-    // text.erase(pos, len);
-    manual_erase(pos, len);
+
+    string deleted = text.substr(pos, len);
+
+    Action act;
+    act.type = "delete";
+    act.position = pos;
+    act.content = deleted;
+    act.length = len;
+    undoStack.push(act);
+    redoStack = stack<Action>();
+
+    text.erase(pos, len);
+    print();
+}
+
+void doCopy(size_t pos, size_t len) {
+    if (!isValidRange(pos, len)) {
+        cout << "Invalid range!" << endl;
+        return;
+    }
+    clipboard = text.substr(pos, len);
+    cout << "Copied: \"" << clipboard << "\"" << endl;
+}
+
+void doPaste(size_t pos) {
+    if (clipboard.empty()) {
+        cout << "Clipboard is empty!" << endl;
+        return;
+    }
+    if (!isValidPosition(pos)) {
+        cout << "Invalid position!" << endl;
+        return;
+    }
+
+    Action act;
+    act.type = "paste";
+    act.position = pos;
+    act.content = clipboard;
+    undoStack.push(act);
+    redoStack = stack<Action>();
+
+    text.insert(pos, clipboard);
+    print();
+}
+
+void doUndo() {
+    if (undoStack.empty()) {
+        cout << "Nothing to undo." << endl;
+        return;
+    }
+
+    Action act = undoStack.top();
+    undoStack.pop();
+    redoStack.push(act);
+
+    if (act.type == "insert" || act.type == "paste") {
+        text.erase(act.position, act.content.size());
+    } else if (act.type == "delete") {
+        text.insert(act.position, act.content);
+    }
+
+    print();
+}
+
+void doRedo() {
+    if (redoStack.empty()) {
+        cout << "Nothing to redo." << endl;
+        return;
+    }
+
+    Action act = redoStack.top();
+    redoStack.pop();
+    undoStack.push(act);
+
+    if (act.type == "insert" || act.type == "paste") {
+        text.insert(act.position, act.content);
+    } else if (act.type == "delete") {
+        text.erase(act.position, act.content.size());
+    }
+
     print();
 }
 
 int main() {
-
     string line;
 
     cout << "========= Simple Text Editor - Command Line =======" << endl;
@@ -63,6 +157,10 @@ int main() {
     cout << "  print                   : Show current text" << endl;
     cout << "  insert <pos> <text>     : Insert text at position" << endl;
     cout << "  delete <pos> <length>   : Delete from position, length characters" << endl;
+    cout << "  copy <pos> <length>     : Copy text to clipboard" << endl;
+    cout << "  paste <pos>             : Paste clipboard content at position" << endl;
+    cout << "  undo                    : Undo last action" << endl;
+    cout << "  redo                    : Redo last undone action" << endl;
     cout << "  exit / quit             : Exit the program" << endl;
     cout << "  help                    : See this again" << endl;
     cout << "---------------------------------------------------" << endl;
@@ -96,12 +194,32 @@ int main() {
             ss >> pos >> len;
             doDelete(pos, len);
         }
+        else if (command == "copy") {
+            size_t pos, len;
+            ss >> pos >> len;
+            doCopy(pos, len);
+        }
+        else if (command == "paste") {
+            size_t pos;
+            ss >> pos;
+            doPaste(pos);
+        }
+        else if (command == "undo") {
+            doUndo();
+        }
+        else if (command == "redo") {
+            doRedo();
+        }
         else if (command == "help") {
             cout << "========= Simple Text Editor - Command Line =======" << endl;
             cout << "--------------- Available commands  ---------------" << endl;
             cout << "  print                  : Show current text" << endl;
             cout << "  insert <pos> <text>     : Insert text at position" << endl;
             cout << "  delete <pos> <length>   : Delete from position, length characters" << endl;
+            cout << "  copy <pos> <length>     : Copy text to clipboard" << endl;
+            cout << "  paste <pos>             : Paste clipboard content at position" << endl;
+            cout << "  undo                    : Undo last action" << endl;
+            cout << "  redo                    : Redo last undone action" << endl;
             cout << "  exit / quit             : Exit the program" << endl;
             cout << "  help                    : See this again" << endl;
             cout << "---------------------------------------------------" << endl;
